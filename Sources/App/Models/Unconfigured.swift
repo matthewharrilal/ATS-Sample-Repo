76f0137fd08b1faecbe1ce0fa44cacd4d83031e2
Matthew Harrilal <matthewharrilal@gmail.com>
2018-10-03 09:59:43 -0700

    Initial commit of the vapor project

---
+++ Sources/App/Models/Unconfigured.swift
@@ -0,0 +1,18 @@
+import FluentSQLite
+import Vapor
+
+/// A single entry of a Todo list.
+final class Unconfigured: SQLiteModel {
+    // Better Do This: Configurable Class ==> Let's get some properties!
+    
+    
+}
+
+/// Better do this: Migrate Uncofigured model as schema
+
+
+/// Allows `Todo` to be encoded to and decoded from HTTP messages.
+extension Todo: Content { }
+
+/// Allows `Todo` to be used as a dynamic parameter in route definitions.
+extension Todo: Parameter { }
