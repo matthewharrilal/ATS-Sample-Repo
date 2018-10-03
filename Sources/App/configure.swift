310fb556fb6c5a495dafa5c039848d15f3b62403
Matthew Harrilal <matthewharrilal@gmail.com>
2018-10-03 15:45:22 -0700

    Added the project

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,7 +1,7 @@
 import FluentPostgreSQL
 import Vapor
 
-/// Called before your application initializes.
+/// Called before your application initializes. (IMPORTANT TO UNDERSTAND)
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
     /// Register providers first
     try services.register(FluentPostgreSQLProvider())
