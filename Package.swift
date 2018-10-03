6c98b0f3a5df516b128cafba8fd4ec5ee39cd15d
Matthew Harrilal <matthewharrilal@gmail.com>
2018-10-03 10:25:46 -0700

    Added the authentication and hashing libraries

--- Package.swift
+++ Package.swift
@@ -8,7 +8,13 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
 
         // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
-         .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0-rc")
+         .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0-rc"),
+         
+         // For Authentication
+         .package(url: "https://github.com/vapor/auth.git", from: "2.0.0-rc"),
+         
+         // For hashing credentials
+         .package(url: "https://github.com/vapor/crypto.git", from: "3.0.0")
     ],
     targets: [
         .target(name: "App", dependencies: ["FluentPostgreSQL", "Vapor"]),
